f8ffb5e7644aa60925473f88348f5497e9aa6fef
Antonio <selvas.leon029@gmail.com>
2025-11-13 01:55:33 -0600

    first commit

---
+++ app/src/main/java/com/antonioselvas/finanzasapp/presentation/views/LoginView.kt
@@ -0,0 +1,2 @@
+package com.antonioselvas.finanzasapp.presentation.views
+
---
+++ app/src/main/java/com/antonioselvas/finanzasapp/presentation/views/RegisterView.kt
@@ -0,0 +1,19 @@
+package com.antonioselvas.finanzasapp.presentation.views
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun RegisterView(){
+    Scaffold {
+        RegisterContent(it)
+    }
+}
+
+
+@Composable
+fun RegisterContent(paddingValues: PaddingValues){
+    
+}
